97d0db8e22468a851f9a2d4a0bc136a4395e7589
Caio Henrique <caiohenrqq@gmail.com>
2024-04-19 14:23:12 -0400

    adiciona implementações sessão hero

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // API IBGE para pegar estados
-fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+fetch('https://servicodados.ibge.gov.br/api/v1/localidades/paises')
   .then(response => response.json())
   .then(data => {
-    let campoLocais = document.getElementById('campoLocais');
+    let campoLocais = document.getElementById('campoPais');
 
     data.forEach(estado => {
       var option = document.createElement('option');
@@ -18,8 +18,32 @@ fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
 
 // JQuery & Select2
 $(document).ready(function () {
-  $('.campoLocais').select2({
-    placeholder: "Selecione um lugar",
+  $('.campoPais').select2({
+    placeholder: "País",
+    allowClear: true,
+
+    // Traduz mensagem ao não encontrar nada.
+    language: {
+      noResults: function () {
+        return 'Nenhum resultado encontrado.';
+      }
+    }
+  });
+
+  $('.campoEstado').select2({
+    placeholder: "Estado",
+    allowClear: true,
+
+    // Traduz mensagem ao não encontrar nada.
+    language: {
+      noResults: function () {
+        return 'Nenhum resultado encontrado.';
+      }
+    }
+  });
+
+  $('.campoCidade').select2({
+    placeholder: "Cidade",
     allowClear: true,
 
     // Traduz mensagem ao não encontrar nada.
@@ -32,20 +56,58 @@ $(document).ready(function () {
 });
 
 // Executa somente após a página ter sido totalmente carregada.
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var checkbox = document.querySelector('.checkbox');
   var checkBoxInput = document.getElementById('checkBox');
+  var textoCheckIn = document.getElementById('textoCheckIn');
 
   // Lógica para quando o usuário clicar na checkbox, liberar o uso da caixa Check-out.
-  checkbox.addEventListener('change', function() {
+  checkbox.addEventListener('change', function () {
     if (!checkbox.checked) {
       checkBoxInput.classList.add('btn-disabled');
+      checkBoxInput.style.display = 'none';
+      textoCheckIn.textContent = "Check-out?";
     } else {
+      checkBoxInput.style.removeProperty('display');
       checkBoxInput.classList.remove('btn-disabled');
+      textoCheckIn.textContent = "Check-out";
     }
   });
 });
 
+// Enviar pro Whatsapp formulário
+function enviaWhatsapp() {
+  const numeroCelular = '+5569992974079';
+  const mensagem = '...';
+
+  const whatsappLink = 'https://wa.me/' + numeroCelular;
+  
+  console.log(whatsappLink);
+}
+
+// Previne botão Reservar de executar qualquer função
+document.addEventListener('DOMContentLoaded', function() {
+  var submitButton = document.querySelector('button[type="submit"]');
+
+  if (submitButton) {
+    submitButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+      console.log('Botão submit clicado, mas o formulário não será enviado.');
+    });
+  } else {
+    console.error('Botão submit não encontrado.');
+  }
+});
+
+  // Função para fechar o alert
+  document.getElementById('fecharReservar').addEventListener('click', function() {
+    document.getElementById('alertReservar').close(); // Fecha o modal
+  });
+
+  // Impedir o envio do formulário ao clicar no botão "Fechar"
+  document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão de envio do formulário
+  });
 
 
 
